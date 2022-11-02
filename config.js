ff3fb896c9219723fc6b6241aee352f77f7bc6ea
lbthoangf <117233823+lbthoangf@users.noreply.github.com>
2022-11-02 20:38:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    introTitle: 'Em yêu  à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    introTitle : 'Em à!' 
+    introDesc : Thường  lệ  vốn  trái  đất
+    Mà  sao  em  cứ  đi  nhầm  đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
